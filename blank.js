2ddd5a422b660bdceea739af9eebdd7459c01998
AcademiccMacharia <machariab19@gmail.com>
2023-05-17 10:00:20 +0300

    JavaScript function to check whether a string is blank or not

---
+++ blank.js
@@ -0,0 +1,10 @@
+//JavaScript function to check whether a string is blank or not
+
+function is_Blank(str) {
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(is_Blank('.'));
